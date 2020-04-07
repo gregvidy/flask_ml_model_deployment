6103bae655b5beb6a5fd1ee5d5ceeed64b501c4c
Gregorius <gregvidy@gmail.com>
2020-04-07 13:07:06 +0700

    add random_state for both rf and extratrees

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -4,10 +4,12 @@ from sklearn import linear_model
 MODELS = {
     "randomforest": ensemble.RandomForestClassifier(n_estimators=100,
                                                     n_jobs=-1,
-                                                    verbose=2),
+                                                    verbose=2,
+                                                    random_state=1997),
     "extratrees": ensemble.RandomForestClassifier(n_estimators=200,
                                                   n_jobs=-1,
-                                                  verbose=2)
+                                                  verbose=2,
+                                                  random_state=123),
     "logreg": linear_model.LogisticRegression(penalty="l2",
                                               C=0.0001,
                                               max_iter=150,
