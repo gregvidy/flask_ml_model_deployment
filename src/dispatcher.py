8142eaf6394d3ba50e5f63da0c0250ef1d491c48
Gregorius <gregvidy@gmail.com>
2020-04-07 12:09:45 +0700

    add new model and tune hyperparameter

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,6 +1,16 @@
 from sklearn import ensemble
+from sklearn import linear_model
 
 MODELS = {
-    "randomforest": ensemble.RandomForestClassifier(n_estimators=100, n_jobs=-1, verbose=2),
-    "extratrees": ensemble.RandomForestClassifier(n_estimators=200, n_jobs=-1, verbose=2)
+    "randomforest": ensemble.RandomForestClassifier(n_estimators=100,
+                                                    n_jobs=-1,
+                                                    verbose=2),
+    "extratrees": ensemble.RandomForestClassifier(n_estimators=200,
+                                                  n_jobs=-1,
+                                                  verbose=2)
+    "logreg": linear_model.LogisticRegression(penalty="l2",
+                                              C=0.0001,
+                                              max_iter=150,
+                                              verbose=2,
+                                              n_jobs=-1)
 }
